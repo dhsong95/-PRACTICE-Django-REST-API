8dd8326363ee0903b87c7736baec0bf530804259
Daehoon Song <dhsong@Song-MacBookPro.local>
2020-02-27 16:16:27 +0900

    change name to chapter01 && initialize chapter02

--- gamesapi/games/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import game_list, game_detail
-
-app_name = 'games'
-
-urlpatterns = [
-    path('', game_list, name='list'),
-    path('<int:pk>/', game_detail, name='detail'),
-]
