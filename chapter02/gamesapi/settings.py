a8d5f69eab548a613eb8b321c3d5262268d7076f
Daehoon Song <dhsong@Song-MacBookPro.local>
2020-02-27 16:53:21 +0900

    ModelSerialzier and api_view decorator

--- chapter02/gamesapi/settings.py
+++ chapter02/gamesapi/settings.py
@@ -120,9 +120,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'Asia/Seoul'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
